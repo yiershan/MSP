c14f1f081993a6dca28e9735b9741d50b2d55a3f
yiershan <511176294@qq.com>
2018-06-28 17:52:20 +0800

    初次提交

---
+++ Code/MSP/MSP.Web/Controllers/TokenController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MSP.Web.Controllers
+{
+    public class TokenController : MSPApiController
+    {
+        [HttpGet]
+        public async Task<JsonResult> GetAsync() {
+            var client = new DiscoveryClient("http://10.3.30.210:1234")
+            {
+                Policy = {
+                RequireHttps = false
+                }
+            };
+            var disco = await client.GetAsync();
+
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "ro.client", "secret1");
+            var tokenResponse = await tokenClient.RequestResourceOwnerPasswordAsync("1111", "password");
+            return new JsonResult(tokenResponse);
+        }
+    }
+    [Route("api/[controller]/[action]")]
+    public class MSPApiController : Controller
+    {
+
+    }
+}
---
+++ Code/MSP/MSP.Web/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace MSP.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "接口文档",
+                });
+            });
+            services.AddMvc();
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseCors(configurePolicy: builder => {
+                builder.WithOrigins() //允许任何来源的主机访问
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "MSP API V1");
+            });
+            app.UseMvc();
+        }
+    }
+}
